86695caca39b35a944dc635088687b051168ecce
dmunter2 <devon.munter@gmail.com>
2019-08-01 19:20:23 -0700

    imgpulled

--- src/components/NasaList.js
+++ src/components/NasaList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 
+// import cssStyles from '../../'
 import axios from 'axios';
 
 import NasaCard from './NasaCard';
@@ -7,32 +8,24 @@ import NasaCard from './NasaCard';
 
 export default function NasaList() {
     const [photos, setPhotos] = useState([]);
-    const [galaxy, setGalaxy] = useState('milkyWay')
-    const [isLoading, setIsLoading] = useState(false);
-    
-
 
     useEffect(() => {
-        setIsLoading(true);
-        setPhotos([]);
         axios
-            .get('https://api.nasa.gov/planetary/apod?api_key=J9kmqCDGVQ2AzU68n8Dcl5BJ1pSoAtgFHkTPgNYr')
-            .then(dataObject => {
-                console.log(dataObject);
-                setPhotos(dataObject.url)
-                setIsLoading(false);
+            .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+            .then(data => {
+                console.log(data);
+                setPhotos(data.data.url)
             })
             .catch(err => {
                 console.log(err);
-                setIsLoading(false);
             })
-    }, []);
-
+    },[]);
     return (
-        <div>
-            {photos.map(photo => {
-                return <NasaCard imgUrl={imgUrl} />
-            })}
+        <div> <NasaCard imgUrl={photos} />
         </div>
+
     );
 }
+
+
+
