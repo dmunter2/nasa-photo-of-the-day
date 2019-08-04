cfc8563f23c681cc141f7faa7beb9340f4d159ad
dmunter2 <devon.munter@gmail.com>
2019-08-04 13:01:30 -0700

    styled-card

--- src/components/NasaList.js
+++ src/components/NasaList.js
@@ -1,27 +1,33 @@
 import React, { useEffect, useState } from "react";
 
+import styled from 'styled-components';
+
 // import cssStyles from '../../'
 import axios from 'axios';
 
 import NasaCard from './NasaCard';
 
 
+
 export default function NasaList() {
-    const [photos, setPhotos] = useState([]);
+    const [photos, setPhotos] = useState();
+    const [titles, setTitles] = useState();
+    const [explanation, setExplanation] = useState();
 
     useEffect(() => {
         axios
-            .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+            .get('https://api.nasa.gov/planetary/apod?api_key=J9kmqCDGVQ2AzU68n8Dcl5BJ1pSoAtgFHkTPgNYr')
             .then(data => {
-                console.log(data);
                 setPhotos(data.data.url)
+                setTitles(data.data.title)
+                setExplanation(data.data.explanation);
             })
             .catch(err => {
                 console.log(err);
             })
     },[]);
     return (
-        <div> <NasaCard imgUrl={photos} />
+        <div> <NasaCard imgUrl={photos} titleInfo= {titles} textInfo={explanation}/>
         </div>
 
     );
---
+++ src/components/api-style.js
@@ -0,0 +1,8 @@
+//This will be the styled img and buttons
+
+// import styled from "styled-components";
+
+// export const Image = styled.img`
+//     max-width: 200px;
+//     border-radius: 10px:    
+// `;
